b79c622f8f7c1c0d280ff7891856c0363f1fadef
Shnnli <Shnnli@163.com>
2023-06-28 16:32:47 +0800

    fix: 🐛 重置设置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,18 +1,34 @@
 import { defineConfig } from 'dumi';
 const repo = 'fast-ui';
 const logo =
-  process.env.NODE_ENV === 'production'
-    ? `/${repo}/logo.png`
-    : '/docs-dist/logo.png';
-
+  process.env.NODE_ENV === 'production' ? `/${repo}/logo.png` : '/logo.png';
 export default defineConfig({
   favicons: [logo],
   outputPath: 'docs-dist',
   title: 'fast-ui',
+  hash: false,
+  resolve: {
+    // docDirs: [{ type: 'doc', dir: 'docs' }],
+    atomDirs: [{ type: 'component', dir: 'components' }],
+    // codeBlockMode: 'passive',
+  },
+  alias: {
+    'fast-ui': require.resolve('./.dumi/fastui.js'),
+  },
   themeConfig: {
     name: 'fast-ui',
     logo: logo,
     rtl: true,
+    socialLinks: {
+      github: 'https://github.com/Shnnli/fast-ui',
+      // weibo: 'https://xxxx',
+      // twitter: 'https://xxxx',
+      // gitlab: 'https://xxxx',
+      // facebook: 'https://xxxx',
+      // zhihu: 'https://xxxx',
+      // yuque: 'https://xxxx',
+      // linkedin: 'https://xxxx',
+    },
     nav: {
       'zh-CN': [
         {
@@ -22,10 +38,6 @@ export default defineConfig({
         {
           title: '组件',
           link: '/components/foo',
-        },
-        {
-          title: 'GitHub',
-          link: 'https://github.com/Shnnli/fast-ui',
         },
       ],
       'en-US': [
@@ -37,16 +49,9 @@ export default defineConfig({
           title: '组件',
           link: '/components/foo',
         },
-        {
-          title: 'GitHub',
-          link: 'https://github.com/Shnnli/fast-ui',
-        },
       ],
     },
     footer:
       "<p> Copyright © 2023 | Made with ❤ by <a href='https://github.com/Shnnli/fast-ui'>Shnnli<a><p>",
   },
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/docs-dist/',
-  publicPath:
-    process.env.NODE_ENV === 'production' ? `/${repo}/` : '/docs-dist/',
 });
