02d86dad88bee43e516c534f881dab1cc6bc7d6d
Shnnli <Shnnli@163.com>
2023-06-30 16:32:07 +0800

    feat: 🎸 发布一个初始包

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    input: 'components',
+    output: 'dist',
+    ignores: ['components/**/demo/**'],
+  },
 });
